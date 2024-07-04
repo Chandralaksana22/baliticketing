a8912b82899e554113e0d78430b1d0bc93b4926e
Trisna Chandra Laksana <57003845+Chandralaksana22@users.noreply.github.com>
2024-07-04 16:29:12 +0800

    Update Input Country

--- src/routes/detailcostumer/+page.svelte
+++ src/routes/detailcostumer/+page.svelte
@@ -109,10 +109,10 @@
 								bind:value={lastname}
 								class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
 								placeholder="Green"
-								required
+								required 
 							/>
 						</div>
-						<div>
+						<div class="sm:col-span-2">
 							<div class="mb-2 flex items-center gap-2">
 								<label
 									for="select-country-input-3"
