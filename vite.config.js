bc74a70f7959f030a719ae8d2455718b87b7be02
Trisna Chandra Laksana <57003845+Chandralaksana22@users.noreply.github.com>
2024-07-05 17:00:11 +0800

    Update Config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import compression from 'vite-plugin-compression';
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		ViteMinifyPlugin({}),
 		compression({
 			algorithm: 'gzip',
 			ext: '.gz',
