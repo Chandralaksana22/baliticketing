6219530f336c48b7cbefff3eba3739228ff7cbe5
Trisna Chandra Laksana <57003845+Chandralaksana22@users.noreply.github.com>
2024-07-06 12:24:19 +0800

    Add Config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-// import compression from 'vite-plugin-compression';
+import compression from 'vite-plugin-compression';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		// compression({
-		// 	algorithm: 'gzip',
-		// 	ext: '.gz',
-		// }),
-		// compression({
-		// 	algorithm: 'brotliCompress',
-		// 	ext: '.br',
-		// }),
+		compression({
+			algorithm: 'gzip',
+			ext: '.gz',
+		}),
+		compression({
+			algorithm: 'brotliCompress',
+			ext: '.br',
+		}),
 	],
 	build: {
-		// minify: 'terser',
+		minify: 'terser',
 		target: 'es2015',
 		cssCodeSplit: true,
 		chunkSizeWarningLimit: 1500,
