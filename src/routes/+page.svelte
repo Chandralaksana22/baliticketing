62a93dc8a8b904d702f1d765edd25ae52f9e2656
Trisna Chandra Laksana <57003845+Chandralaksana22@users.noreply.github.com>
2024-07-19 13:49:23 +0800

    Update : Update Homepage Device

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,18 +6,32 @@
 	import BaliZooHome from '$lib/components/Section/BaliZooHome.svelte';
 	import BaliBirdParkHome from '$lib/components/Section/BaliBirdParkHome.svelte';
 	import BaliSafariHome from '$lib/components/Section/BaliSafariHome.svelte';
-    import OpeningHourModal from '$lib/components/Modal/OpeningHour.svelte'
-    import ZooMapModalHome from '$lib/components/Modal/ZooMapModalHome.svelte';
-    import GeneralAdmissionModal from '$lib/components/Modal/GeneralAdmissionModal.svelte'
+	import OpeningHourModal from '$lib/components/Modal/OpeningHour.svelte';
+	import ZooMapModalHome from '$lib/components/Modal/ZooMapModalHome.svelte';
+	import GeneralAdmissionModal from '$lib/components/Modal/GeneralAdmissionModal.svelte';
 	import WhatsOnModal from '$lib/components/Modal/WhatsOnModalHome.svelte';
+	import { onMount } from 'svelte';
+	let isMobile = false;
+
+	function checkDevice() {
+		isMobile = window.innerWidth <= 1023;
+	}
+
+	onMount(() => {
+		checkDevice();
+		window.addEventListener('resize', checkDevice);
+
+		return () => {
+			window.removeEventListener('resize', checkDevice);
+		};
+	});
 	export let data: PageServerData;
 
 	let vendor = data?.vendor;
-    let balifarm = data?.balifarm;
-    let balizoo = data?.balizoo;
-    let balibird = data?.balibird
-    let balisafari = data?.balisafari
-	
+	let balifarm = data?.balifarm;
+	let balizoo = data?.balizoo;
+	let balibird = data?.balibird;
+	let balisafari = data?.balisafari;
 </script>
 
 <section class="bg-cover bg-[url('/images/bg-desktop.png')]">
@@ -39,63 +53,84 @@
 			</h1>
 		</div>
 	</div>
-	
+
 	<div class="px-5 pb-0 lg:pb-44 lg:px-10 container mx-auto">
 		<div id="feature">
 			<h5 class="text-center font-bold text-white text-2xl boldfont">Places To Go</h5>
-			<div class="grid lg:hidden grid-cols-1 lg:grid-cols-12 py-5 gap-6">
-				<VendorHomeMobile data={vendor} />
-			</div>
-			<div class="hidden lg:grid grid-cols-1 lg:grid-cols-12 py-5 gap-6">
-				<VendorHome data={vendor} />
-			</div>
+			{#if isMobile}
+				<div class="grid grid-cols-1 lg:grid-cols-12 py-5 gap-6">
+					<VendorHomeMobile data={vendor} />
+				</div>
+			{:else}
+				<div class="grid grid-cols-1 lg:grid-cols-12 py-5 gap-6">
+					<VendorHome data={vendor} />
+				</div>
+			{/if}
 		</div>
 		<div>
-			<div
-				class="hidden lg:block panel-home bg-[#FFD600] p-5 lg:p-10 rounded-3xl mt-10 lg:mt-44 mb-[-20%] relative"
-			>
-				<div class="grid grid-cols-2 lg:grid-cols-4 gap-4">
-                    <OpeningHourModal data={vendor}/>
-                    <GeneralAdmissionModal explorerGA={balizoo} balifarmGA={balifarm} balisafariGA={balisafari} balibirdGA={balibird}/>
-                    <ZooMapModalHome data={vendor}/>
-                    <WhatsOnModal explorerWO={balizoo} balifarmWO={balifarm} balisafariWO={balisafari} balibirdWO={balibird}/>
-                </div>
-			</div>
+			<!-- svelte-ignore empty-block -->
+			{#if isMobile}{:else}
+				<div
+					class="block panel-home bg-[#FFD600] p-5 lg:p-10 rounded-3xl mt-10 lg:mt-44 mb-[-20%] relative"
+				>
+					<div class="grid grid-cols-2 lg:grid-cols-4 gap-4">
+						<OpeningHourModal data={vendor} />
+						<GeneralAdmissionModal
+							explorerGA={balizoo}
+							balifarmGA={balifarm}
+							balisafariGA={balisafari}
+							balibirdGA={balibird}
+						/>
+						<ZooMapModalHome data={vendor} />
+						<WhatsOnModal
+							explorerWO={balizoo}
+							balifarmWO={balifarm}
+							balisafariWO={balisafari}
+							balibirdWO={balibird}
+						/>
+					</div>
+				</div>
+			{/if}
 		</div>
 	</div>
 </section>
-<section
-	class="hidden lg:block bg-cover h-[25vh] lg:h-auto bg-[url('/images/background-biru-desktop.png')]"
->
-	<div class="hidden lg:grid grid-cols-1 lg:grid-cols-12 gap-4">
-		<div class="col-span-7 pt-32">
-			<h4 class="boldfont text-white text-3xl text-center">Explore Bali Zoo</h4>
-			<div class="py-20 mx-auto px-10">
-				<BaliZooHome data={balizoo}/>
+<!-- svelte-ignore empty-block -->
+{#if isMobile}{:else}
+	<section
+		class="block bg-cover h-[25vh] lg:h-auto bg-[url('/images/background-biru-desktop.png')]"
+	>
+		<div class="grid grid-cols-1 lg:grid-cols-12 gap-4">
+			<div class="col-span-7 pt-32">
+				<h4 class="boldfont text-white text-3xl text-center">Explore Bali Zoo</h4>
+				<div class="py-20 mx-auto px-10">
+					<BaliZooHome data={balizoo} />
+				</div>
 			</div>
-		</div>
-		<div class="col-span-5 bg-[url('/images/plan.png')]">
-			<div class="pt-32">
-				<h4 class="boldfont text-white text-3xl text-center">
-					A WELL SPENT DAY <br /> at BALI BIRD PARK
-				</h4>
-				<div class="py-20 px-10">
-					<BaliBirdParkHome data={balibird}/>
+			<div class="col-span-5 bg-[url('/images/plan.png')]">
+				<div class="pt-32">
+					<h4 class="boldfont text-white text-3xl text-center">
+						A WELL SPENT DAY <br /> at BALI BIRD PARK
+					</h4>
+					<div class="py-20 px-10">
+						<BaliBirdParkHome data={balibird} />
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
-<section>
-    <div class="hidden lg:grid grid-cols-1 lg:grid-cols-12">
-        <div class="col-span-5">
-            <BaliFarmHome data={balifarm}/>
-        </div>
-        <div class="col-span-7 bg-contain bg-[url('/images/bg-hijau.png')]">
-            <h4 class="boldfont pt-24  text-white text-3xl text-center">WILDLIFE ADVENTURE AT BALI SAFARI</h4>
-            <div class="pt-10 mx-auto px-10">
-             <BaliSafariHome data={balisafari}/>
-            </div>
-        </div>
-    </div>
-</section>
+	</section>
+	<section>
+		<div class="grid grid-cols-1 lg:grid-cols-12">
+			<div class="col-span-5">
+				<BaliFarmHome data={balifarm} />
+			</div>
+			<div class="col-span-7 bg-contain bg-[url('/images/bg-hijau.png')]">
+				<h4 class="boldfont pt-24 text-white text-3xl text-center">
+					WILDLIFE ADVENTURE AT BALI SAFARI
+				</h4>
+				<div class="pt-10 mx-auto px-10">
+					<BaliSafariHome data={balisafari} />
+				</div>
+			</div>
+		</div>
+	</section>
+{/if}
