acf44f50057affa4737c7fa27ce101ba2f108da6
Trisna Chandra Laksana <57003845+Chandralaksana22@users.noreply.github.com>
2024-07-26 18:02:17 +0800

    Update Layout No Data

--- src/lib/components/Modal/DetailVendor/WhatsOnModalHome.svelte
+++ src/lib/components/Modal/DetailVendor/WhatsOnModalHome.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	export let explorerWO: any = [];
+	const filteredWO = explorerWO.filter((product) => product?.type === "What's On");
 </script>
 
 <button
@@ -26,21 +27,38 @@
 	<div class="modal-box w-11/12 max-w-5xl">
 		<div class="relative bg-white p-5 rounded-xl">
 			<div class="flex items-center justify-between border-b rounded-t dark:border-gray-600">
-				<div class="flex items-center justify-between border-b rounded-t dark:border-gray-600">
-					<h3 class="text-xl text-center boldfont lg:text-start font-semibold text-blue">
-						What's On
-					</h3>
-					<form method="dialog">
-						<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-					</form>
-				</div>
+				<h3 class="text-xl text-center boldfont lg:text-start font-semibold text-blue">
+					What's On
+				</h3>
+				<form method="dialog">
+					<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+				</form>
 			</div>
 			<div class="p-4 md:p-5 space-y-4">
-				{#each explorerWO as product}
-					{#if product?.type === "What's On"}
-						<div
-							class="overflow-y-auto h-[50vh] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 WOp-2 w-full"
-						>
+				{#if filteredWO.length === 0}
+					<div class="flex items-center justify-center p-4 h-[50vh] mx-auto rounded-lg bg-gray-100">
+						<div class="text-center">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="101"
+								height="100"
+								viewBox="0 0 101 100"
+								fill="none"
+								class="mx-auto mb-4"
+							>
+								<path
+									d="M81.75 12.5H72.375V9.375C72.375 8.5462 72.0458 7.75134 71.4597 7.16529C70.8737 6.57924 70.0788 6.25 69.25 6.25C68.4212 6.25 67.6263 6.57924 67.0403 7.16529C66.4542 7.75134 66.125 8.5462 66.125 9.375V12.5H34.875V9.375C34.875 8.5462 34.5458 7.75134 33.9597 7.16529C33.3737 6.57924 32.5788 6.25 31.75 6.25C30.9212 6.25 30.1263 6.57924 29.5403 7.16529C28.9542 7.75134 28.625 8.5462 28.625 9.375V12.5H19.25C17.5924 12.5 16.0027 13.1585 14.8306 14.3306C13.6585 15.5027 13 17.0924 13 18.75V81.25C13 82.9076 13.6585 84.4973 14.8306 85.6694C16.0027 86.8415 17.5924 87.5 19.25 87.5H81.75C83.4076 87.5 84.9973 86.8415 86.1694 85.6694C87.3415 84.4973 88 82.9076 88 81.25V18.75C88 17.0924 87.3415 15.5027 86.1694 14.3306C84.9973 13.1585 83.4076 12.5 81.75 12.5ZM28.625 18.75V21.875C28.625 22.7038 28.9542 23.4987 29.5403 24.0847C30.1263 24.6708 30.9212 25 31.75 25C32.5788 25 33.3737 24.6708 33.9597 24.0847C34.5458 23.4987 34.875 22.7038 34.875 21.875V18.75H66.125V21.875C66.125 22.7038 66.4542 23.4987 67.0403 24.0847C67.6263 24.6708 68.4212 25 69.25 25C70.0788 25 70.8737 24.6708 71.4597 24.0847C72.0458 23.4987 72.375 22.7038 72.375 21.875V18.75H81.75V31.25H19.25V18.75H28.625ZM81.75 81.25H19.25V37.5H81.75V81.25Z"
+									fill="#BABABA"
+								/>
+							</svg>
+							<p class="text-gray-700">No data available for the selected product.</p>
+						</div>
+					</div>
+				{:else}
+					<div
+						class="overflow-y-auto h-[50vh] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 WOp-2 w-full"
+					>
+						{#each filteredWO as product}
 							<div>
 								<a data-sveltekit-reload href={`/vendor/${product?.vendor?.slug}/${product?.slug}`}>
 									<img
@@ -53,7 +71,6 @@
 									<h5 class="mb-2 text-md font-bold tracking-tight truncate text-gray-900">
 										{product?.name}
 									</h5>
-									<!-- Replace NuxtLink with Svelte Link equivalent -->
 									<a
 										data-sveltekit-reload
 										href={`/vendor/${product?.vendor?.slug}/${product?.slug}`}
@@ -61,30 +78,9 @@
 									>
 								</div>
 							</div>
-						</div>
-					{:else}
-						<div
-							class="flex items-center justify-center p-4 h-[50vh] mx-auto rounded-lg bg-gray-100"
-						>
-							<div class="text-center">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="101"
-									height="100"
-									viewBox="0 0 101 100"
-									fill="none"
-									class="mx-auto mb-4"
-								>
-									<path
-										d="M81.75 12.5H72.375V9.375C72.375 8.5462 72.0458 7.75134 71.4597 7.16529C70.8737 6.57924 70.0788 6.25 69.25 6.25C68.4212 6.25 67.6263 6.57924 67.0403 7.16529C66.4542 7.75134 66.125 8.5462 66.125 9.375V12.5H34.875V9.375C34.875 8.5462 34.5458 7.75134 33.9597 7.16529C33.3737 6.57924 32.5788 6.25 31.75 6.25C30.9212 6.25 30.1263 6.57924 29.5403 7.16529C28.9542 7.75134 28.625 8.5462 28.625 9.375V12.5H19.25C17.5924 12.5 16.0027 13.1585 14.8306 14.3306C13.6585 15.5027 13 17.0924 13 18.75V81.25C13 82.9076 13.6585 84.4973 14.8306 85.6694C16.0027 86.8415 17.5924 87.5 19.25 87.5H81.75C83.4076 87.5 84.9973 86.8415 86.1694 85.6694C87.3415 84.4973 88 82.9076 88 81.25V18.75C88 17.0924 87.3415 15.5027 86.1694 14.3306C84.9973 13.1585 83.4076 12.5 81.75 12.5ZM28.625 18.75V21.875C28.625 22.7038 28.9542 23.4987 29.5403 24.0847C30.1263 24.6708 30.9212 25 31.75 25C32.5788 25 33.3737 24.6708 33.9597 24.0847C34.5458 23.4987 34.875 22.7038 34.875 21.875V18.75H66.125V21.875C66.125 22.7038 66.4542 23.4987 67.0403 24.0847C67.6263 24.6708 68.4212 25 69.25 25C70.0788 25 70.8737 24.6708 71.4597 24.0847C72.0458 23.4987 72.375 22.7038 72.375 21.875V18.75H81.75V31.25H19.25V18.75H28.625ZM81.75 81.25H19.25V37.5H81.75V81.25Z"
-										fill="#BABABA"
-									/>
-								</svg>
-								<p class="text-gray-700">No data available for the selected product.</p>
-							</div>
-						</div>
-					{/if}
-				{/each}
+						{/each}
+					</div>
+				{/if}
 			</div>
 		</div>
 	</div>
